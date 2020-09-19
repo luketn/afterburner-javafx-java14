8e5f7441cb100c627cfa466048b7804a28837425
Luke Thompson <endeavour9@gmail.com>
2020-09-19 22:47:20 +1000

    another stab at it based on this stack overflow: https://stackoverflow.com/questions/19924852/autocomplete-combobox-in-javafx/47933342#47933342
    Still don't like this - too complex

--- src/main/java/com/mycodefu/dashboard/tables/TablesPresenter.java
+++ src/main/java/com/mycodefu/dashboard/tables/TablesPresenter.java
@@ -4,12 +4,16 @@ import com.mycodefu.controls.AutocompleteComboBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.util.StringConverter;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+
+import static com.mycodefu.controls.AutocompleteComboBox.createComboBoxWithAutoCompletionSupport;
 
 public class TablesPresenter implements Initializable {
 
@@ -18,13 +22,33 @@ public class TablesPresenter implements Initializable {
 
     @FXML
     AutocompleteComboBox people;
+    @FXML
+    VBox rightControls;
+
+    ComboBox<AutocompleteComboBox.HideableItem<String>> comboBoxWithAutoCompletionSupport;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        people.initAutocompleteWithItems(Arrays.asList("Luke", "Bob", "Jane"));
+        this.comboBoxWithAutoCompletionSupport = createComboBoxWithAutoCompletionSupport(Arrays.asList("Luke", "Bob", "Jane"), new StringConverter<AutocompleteComboBox.HideableItem<String>>() {
+            @Override
+            public String toString(AutocompleteComboBox.HideableItem<String> object) {
+                if (object != null) {
+                    return object.getObject();
+                } else {
+                    return null;
+                }
+            }
+
+            @Override
+            public AutocompleteComboBox.HideableItem<String> fromString(String string) {
+                return new AutocompleteComboBox.HideableItem<>(string, this);
+            }
+        });
+        comboBoxWithAutoCompletionSupport.setEditable(true);
+        rightControls.getChildren().add(comboBoxWithAutoCompletionSupport);
     }
 
     public void addPerson(ActionEvent actionEvent) {
-        dataTable.getItems().add(new TableRowData(people.getEditor().getText(), "Buddy"));
+        dataTable.getItems().add(new TableRowData(this.comboBoxWithAutoCompletionSupport.getEditor().getText(), "Buddy"));
     }
 }
