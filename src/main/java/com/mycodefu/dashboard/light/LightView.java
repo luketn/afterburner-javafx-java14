74c9f42590ce15c0436f575c89046269769f02cd
Luke Thompson <endeavour9@gmail.com>
2020-09-21 00:26:58 +1000

    learn some fxml css

--- src/main/java/com/mycodefu/dashboard/light/LightView.java
+++ src/main/java/com/mycodefu/dashboard/light/LightView.java
@@ -1,23 +1,13 @@
 package com.mycodefu.dashboard.light;
 
-import java.util.function.Function;
-
 import com.mycodefu.afterburner.views.FXMLView;
-import javafx.scene.Parent;
+
+import java.util.function.Function;
 
 public class LightView extends FXMLView {
 
     public LightView(Function<String, Object> injectionContext) {
         super(injectionContext);
-    }
-
-    @Override
-    public Parent getView() {
-        try {
-            Thread.sleep((long) (Math.random() * 100));
-        } catch (InterruptedException ex) {
-        }
-        return super.getView(); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
