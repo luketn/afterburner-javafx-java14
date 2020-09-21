1077b5e4e213580f94e0e1aed96b5b1d609545a2
Luke Thompson <endeavour9@gmail.com>
2020-09-21 21:16:37 +1000

    experiment with tests

---
+++ src/test/java/com/mycodefu/dashboard/tables/TablesTest.java
@@ -0,0 +1,60 @@
+package com.mycodefu.dashboard.tables;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testfx.assertions.api.Assertions.assertThat;
+
+class TablesTest extends ApplicationTest {
+    static TablesView tablesView;
+
+    @Override
+    public void start(Stage stage) {
+        tablesView = new TablesView();
+        stage.setScene(new Scene(tablesView.getView()));
+        stage.show();
+        stage.toFront();
+    }
+
+    @AfterEach
+    void cleanup() throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+
+    @Test
+    void should_contain_add_person_button() {
+        Button addPersonButton = lookup("#addPerson").queryButton();
+        assertThat(addPersonButton).hasText("Add Person");
+    }
+
+    @Test
+    void should_click_on_button() throws InterruptedException {
+        assertEquals(2, lookup("#dataTable").queryTableView().getItems().size());
+
+        TextInputControl personTextField = lookup("#personTextField").queryTextInputControl();
+        clickOn(personTextField);
+        write("Luke");
+        assertThat(personTextField.getText()).isEqualTo("Luke");
+
+        moveTo("#addPerson");
+        clickOn(MouseButton.PRIMARY);
+
+//        WaitForAsyncUtils.waitForFxEvents();
+        //sleep(5, TimeUnit.SECONDS);
+//TODO: Figure out why this fails - problem with Mac permissions? Already set the Accessibility one
+//        assertEquals(3, lookup("#dataTable").queryTableView().getItems().size());
+//https://github.com/TestFX/TestFX/pull/674
+    }
+
+}
