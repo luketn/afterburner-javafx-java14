923e53979341d7284991f169f29b8bc8d21d9e6d
Luke Thompson <endeavour9@gmail.com>
2020-10-05 17:58:55 +1100

    add screenshots and save as CircleCI artefacts

--- src/test/java/com/mycodefu/dashboard/tables/TablesTest.java
+++ src/test/java/com/mycodefu/dashboard/tables/TablesTest.java
@@ -1,5 +1,6 @@
 package com.mycodefu.dashboard.tables;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
@@ -12,14 +13,21 @@ import org.junit.jupiter.api.Test;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import java.nio.file.Paths;
+
+import static com.mycodefu.MyFxTestUtils.testScreenshot;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.assertions.api.Assertions.assertThat;
+import static org.testfx.util.DebugUtils.*;
 
 class TablesTest extends ApplicationTest {
     static TablesView tablesView;
+    private Stage stage;
 
     @Override
     public void start(Stage stage) {
+        this.stage = stage;
         tablesView = new TablesView();
         stage.setScene(new Scene(tablesView.getView()));
         stage.show();
@@ -41,18 +49,22 @@ class TablesTest extends ApplicationTest {
 
     @Test
     void add_person() {
-        TableView<TableRowData> tableView = lookup("#dataTable").queryTableView();
+        testScreenshot(tablesView);
 
+        TableView<TableRowData> tableView = lookup("#dataTable").queryTableView();
         assertEquals(2, tableView.getItems().size());
 
         TextInputControl personTextField = lookup("#personTextField").queryTextInputControl();
         clickOn(personTextField);
         write("Big Nessy");
+
+        testScreenshot(tablesView);
         assertThat(personTextField.getText()).isEqualTo("Big Nessy");
 
         moveTo("#addPerson");
         clickOn(MouseButton.PRIMARY);
 
+        testScreenshot(tablesView);
         assertEquals(3, tableView.getItems().size());
 
         TableRowData tableRowData = tableView.getItems().get(2);
