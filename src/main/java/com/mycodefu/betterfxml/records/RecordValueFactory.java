e2205402628915f6f6702032d84d5b9db151769b
Luke Thompson <endeavour9@gmail.com>
2020-10-06 20:16:32 +1100

    simplify the RecordValueFactory

--- src/main/java/com/mycodefu/betterfxml/records/RecordValueFactory.java
+++ src/main/java/com/mycodefu/betterfxml/records/RecordValueFactory.java
@@ -1,32 +1,26 @@
 package com.mycodefu.betterfxml.records;
 
 import javafx.beans.InvalidationListener;
-import javafx.beans.NamedArg;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.util.Callback;
 
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
  * TableView cell value factory supporting Java immutable Record type values.
  */
 public class RecordValueFactory<S, T> implements Callback<CellDataFeatures<S, T>, ObservableValue<T>> {
-    private Class<?> columnClass;
-    private java.lang.invoke.MethodHandle method;
+    private MethodHandle methodHandle;
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public ObservableValue<T> call(CellDataFeatures<S, T> param) {
-        return getCellDataFromRecordReflectively(param.getTableColumn().getId(), param.getValue());
-    }
+        String id = param.getTableColumn().getId();
+        S rowData = param.getValue();
 
-    private ObservableValue<T> getCellDataFromRecordReflectively(String id, S rowData) {
         if (id == null || id.isEmpty() || rowData == null) return null;
 
         if (id.endsWith("Column")) {
@@ -35,14 +29,15 @@ public class RecordValueFactory<S, T> implements Callback<CellDataFeatures<S, T>
 
         ObservableValue<T> result;
         try {
-            if (columnClass == null) {
-                this.columnClass = rowData.getClass();
-            }
-            if (this.method == null) {
-                this.method = MethodHandles.lookup().unreflect(this.columnClass.getDeclaredMethod(id));
+            if (this.methodHandle == null) {
+                Class<?> columnClass = rowData.getClass();
+                Method declaredMethod = columnClass.getDeclaredMethod(id);
+                MethodHandle methodHandle = MethodHandles.lookup().unreflect(declaredMethod);
+
+                this.methodHandle = methodHandle;
             }
 
-            T value = (T) this.method.invoke(rowData);
+            T value = (T) this.methodHandle.invoke(rowData);
 
             result =  new SimpleValue<>(value);
 
