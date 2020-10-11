20f1e221a7e17c221b5b525e21a532713aaf7c18
Luke Thompson <endeavour9@gmail.com>
2020-10-11 16:00:40 +1100

    add script for tagged release

---
+++ script/release.sh
@@ -0,0 +1,7 @@
+# This is how to release a version - update the version number, e.g. ./release 0.0.1
+VERSION=${1:?no version supplied}
+
+echo "Publishing tagged version v${VERSION}..."
+
+git tag -a "v${VERSION}" -m "Release v${VERSION}"
+git push origin "v${VERSION}"
