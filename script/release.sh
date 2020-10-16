f27e8d85ff82502f642d721e815cd73616aaa120
Luke Thompson <endeavour9@gmail.com>
2020-10-16 13:38:37 +1100

    fix script [skip ci]

--- script/release.sh
+++ script/release.sh
@@ -5,5 +5,5 @@ VERSION=${1:?no version supplied}
 
 echo "Publishing tagged version ${VERSION}..."
 
-git tag -a "v${VERSION}" -m "Release ${VERSION}"
+git tag -a "${VERSION}" -m "Release ${VERSION}"
 git push origin "${VERSION}"
